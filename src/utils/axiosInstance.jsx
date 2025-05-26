cee1f5bd9507f508b47db77c8b151f4bb727b692
SoumikGhosh111 <soumikbuie2001@gmail.com>
2025-05-26 19:47:47 +0530

    changed the base url

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({ 
-    baseURL: "http://localhost:8000", 
-    // baseURL: "https://kickstart-59ea.onrender.com", 
+    // baseURL: "http://localhost:8000", 
+    baseURL: "https://kickstart-59ea.onrender.com", 
     withCredentials: true, // important for cookies
 }); 
 export default axiosInstance; 
