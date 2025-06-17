7d16e82b4f12a8590dc43e5ccbf5fe96a7036c06
SoumikGhosh111 <soumikbuie2001@gmail.com>
2025-06-17 09:50:06 +0530

    omit static.json, debuggin cookie issue

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({ 
     // baseURL: "http://localhost:8000", 
     baseURL: "https://kickstart-59ea.onrender.com", 
-    withCredentials: true, // important for cookies
+    // withCredentials: true, // important for cookies
+    credentials: 'include'
 }); 
 export default axiosInstance; 
