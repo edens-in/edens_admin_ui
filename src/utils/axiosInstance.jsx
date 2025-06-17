2b4ad3e7fa1f0cac626465a4759549efd3bf4980
SoumikGhosh111 <soumikbuie2001@gmail.com>
2025-06-17 09:53:31 +0530

    tring debugging the cookie issue

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -4,6 +4,6 @@ const axiosInstance = axios.create({
     // baseURL: "http://localhost:8000", 
     baseURL: "https://kickstart-59ea.onrender.com", 
     // withCredentials: true, // important for cookies
-    credentials: 'include'
+    credentials: true,
 }); 
 export default axiosInstance; 
